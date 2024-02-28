e44300f809a1271167dd6a0e16d838818ae97740
Juma rajab <jmuhombolage@gmail.com>
2024-02-28 15:00:49 +0300

    complete listening to dynamic port

--- app.js
+++ app.js
@@ -88,6 +88,6 @@ app.get("/post/:id", function (req, res) {
     });
 });
 
-app.listen( 3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log("listen to port 3000");
 });
